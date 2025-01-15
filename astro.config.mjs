8cb01700fb1e8709004581f6f1bd05bdfba354dc
Usman Kolawole <morayo0x00@gmail.com>
2025-01-15 02:11:59 +0100

    modify the deploy.yml file to import the icon folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import icon from "astro:icon";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://morayo0x.github.io",
